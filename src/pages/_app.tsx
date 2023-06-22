8c7e01629fe73c270be0b89bcdf17371be382268
Arkan Aulia Farhan <32859227+arkanaulia@users.noreply.github.com>
2023-06-23 02:39:33 +0700

    maze insert

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,15 +10,37 @@ import "styles/Contact.css";
 import "react-calendar/dist/Calendar.css";
 import "styles/MiniCalendar.css";
 import Head from "next/head";
-import Script from "next/script";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Head>
-        <Script src="/utils/maze.js" />
-        
+        <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function (m, a, z, e) {
+          var s, t;
+          try {
+            t = m.sessionStorage.getItem('maze-us');
+          } catch (err) {}
+
+          if (!t) {
+            t = new Date().getTime();
+            try {
+              m.sessionStorage.setItem('maze-us', t);
+            } catch (err) {}
+          }
+
+          s = a.createElement('script');
+          s.src = z + '?t=' + t + '&apiKey=' + e;
+          s.async = true;
+          a.getElementsByTagName('head')[0].appendChild(s);
+          m.mazeUniversalSnippetApiKey = e;
+        })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'a5ebfa0c-5b47-49d5-88ed-a65b46515fc3');
+      `,
+    }}
+  />
         <title>myITS Wali</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
